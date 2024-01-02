7b384cf858a8d3ca65c1bfac7be76afdad0db995
Aakriti Goyal <82526001+AakritiGoyal12458@users.noreply.github.com>
2024-01-01 22:34:45 -1200

    Update main.tf

--- Var/2-var/main.tf
+++ Var/2-var/main.tf
@@ -1,3 +1,4 @@
+#variables are craeted for those values that keep on changing with time eg. os name , os ip ,etc. u need to identify acc to your code 
 # x=5
 # variables are of diff type : UDV:1. user defined variable , 2. internal system defined variable , 3. provider variables . 
 # UDV - x
@@ -21,7 +22,14 @@ output "your_answer_2_is" {
 
 
 
-#after completion of code to exeute teh file type terraform.exe apply in cmd . 
-#UDV:1. user defined variable : we can call them /give reference using var keyword eg var.x where v is a UDV. 
-#2. internal system defined variable : module
-#3. provider variables : any variable given by provider like aws cloud are calleld attribute variable . 
+/*
+after completion of code to exeute teh file type terraform.exe apply in cmd . 
+terraform apply -> promates us with a option of yes/no to make changes ...in order to make it non interactive w ecan use commannd 
+terraform apply -auto-approve 
+UDV:1. user defined variable : we can call them /give reference using var keyword eg var.x where v is a UDV. 
+2. internal system defined variable : module
+3. provider variables : any variable given by provider like aws cloud are calleld attribute references ...
+at time of craeting code they are called key value pair . eg we have resource block and we gave name to that resource block along with varibale 
+eg resource "aws_instace" "os1" { } this variable os1 store all teh attribute references
+
+*/
