56e51f6bd44059e98660082862d586865189f6ac
Aakriti Goyal <82526001+AakritiGoyal12458@users.noreply.github.com>
2024-01-02 15:34:47 +0530

    Update main.tf
    Variables in Terraform

--- Var/2-var/main.tf
+++ Var/2-var/main.tf
@@ -1,12 +1,13 @@
 # x=5
-
+# variables are of diff type : UDV:1. user defined variable , 2. internal system defined variable , 3. provider variables . 
 # UDV - x
+
 variable "x" {
 	default = 5
 }
 
-#print(x)
-#print("x")
+#print(x) --> anything without " " : variable
+#print("x") --> anything in " " is a string 
 
 
 output "your_answer_1_is" {
@@ -15,5 +16,12 @@ output "your_answer_1_is" {
 
 output "your_answer_2_is" {
 	value = "hi this is ${var.x} bye"
+	#${var.x} : to use variable between the string : string intropolation 
 }
 
+
+
+#after completion of code to exeute teh file type terraform.exe apply in cmd . 
+#UDV:1. user defined variable : we can call them /give reference using var keyword eg var.x where v is a UDV. 
+#2. internal system defined variable : module
+#3. provider variables : any variable given by provider like aws cloud are calleld attribute variable . 
